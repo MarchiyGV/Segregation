3ba5570951d06609ca854205b5e042eca83298f0
Georgiy-8 <Marchiy.Georgiy@yandex.ru>
2022-02-18 17:14:47 +0300

    surface thermal relax reorganised; you should reorganise others

--- scripts/plot_surface_thermal_relax.py
+++ scripts/plot_surface_thermal_relax.py
@@ -1,22 +1,38 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
-file = "surface_thermal_relax.txt"
-df = pd.read_csv(file, sep=';', comment='#', names=['t','T', 'pe'])
+import sys
+name = sys.argv[1]
+n = int(sys.argv[2])
+file = f"../GB_projects/{name}/thermo_output/surface_thermal_relax.txt"
+print(file)
+df = pd.read_csv(file, sep=';', comment='#', names=['t', 'T', 'pe'])
 t = df['t']
 pe = df['pe']
 T = df['T']
 
+def rolling_mean(numbers_series, window_size):
+    windows = numbers_series.rolling(window_size)
+    moving_averages = windows.mean()
+    moving_averages_list = moving_averages.tolist()
+    final_list = moving_averages_list[window_size - 1:]
+    return final_list
+
+pe1 = rolling_mean(pe, n)
+t1 = t[len(pe)-len(pe1):]
+t = np.array(t)
+
 f, (ax1, ax3) = plt.subplots(1, 2)
-x1 = 200
-x2 = -1
+
 ax1.plot(t, pe)
-ax1.axvline(x=t[x1], linestyle='--', color='grey')
 ax1.set_xlabel('$t, ps$')
 ax1.set_ylabel('$E_{pot}, eV$')
 
-ax3.plot(t[x1:x2], pe[x1:x2])
+ax3.plot(t1, pe1)
 ax3.set_xlabel('$t, ps$')
-ax3.set_ylabel('$E_{pot}, eV$')
+ax3.set_ylabel('$<E_{pot}>_{roll}, eV$')
+ax3.set_title(f'rolling mean over {n}')
+f.suptitle(f'Surface thermal relax {name} {round(t[-1])}ps')
 f.tight_layout()
-plt.savefig('plot.surface_thermal_relax.png')
+plt.savefig(f'../GB_projects/{name}/images/plot.surface_thermal_relax_time{round(t[-1])}.png')
+
--- surface_thermal_relax_GB.sh
+++ surface_thermal_relax_GB.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
-
+verbose=false
+mean_width=50
+while getopts v:n:s:m: flag
+do
+    case "${flag}" in
+        v) verbose=true;;
+        n) name=${OPTARG};;
+        s) structure=${OPTARG};;
+        m) mean_width=${OPTARG};;
+        *) echo "Unknonwn option ${flag}"; exit 1;;
+    esac
+done
+if [[ -n "$CONDA_PREFIX" ]]; then
+    echo "Conda is active: $CONDA_PREFIX"
+else
+    read -p "Conda is not active, do you want to continue? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Ok"
+    else
+        exit 1
+    fi
+fi
 echo; echo "Starting LAMMPS procedure..."; echo;
 
-
-lmp_omp_edited -in in.surface_thermal_relax -var T $1 -var initial_relaxation_steps $2
+cd scripts
+if [ $verbose = true ]; then
+    lmp_omp_edited -in in.surface_thermal_relax -var gbname $name -var structure_name $structure
+else
+    $(lmp_omp_edited -in in.surface_thermal_relax -var gbname $name -var structure_name $structure)
+fi
 
 echo; echo "LAMMPS task done, plotting..."; echo
 
-python plot_surface_thermal_relax.py
+python plot_surface_thermal_relax.py $name $mean_width
 
+cd ..
 echo; echo "All done"; echo
