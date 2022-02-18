02cb539d19cc3d9ef6bd12a4c7fd411cfb7fce3b
Georgiy-8 <Marchiy.Georgiy@yandex.ru>
2022-02-18 18:00:06 +0300

    reorganize segregation, but it still should be stopped by user

--- segregation_GB.sh
+++ segregation_GB.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
+verbose=false
+mean_width=50
+conc=-1
+kappa=-1
+while getopts v:n:s:mw:conc:kappa: flag
+do
+    case "${flag}" in
+        v) verbose=true;;
+        n) name=${OPTARG};;
+        s) structure=${OPTARG};;
+        mw) mean_width=${OPTARG};;
+        conc) conc=${OPTARG};;
+        kappa) kappa=${OPTARG};;
+        *) echo "Unknonwn option ${flag}"; exit 1;;
+    esac
+done
+if [[ -n "$CONDA_PREFIX" ]]; then
+    echo "Conda is active: $CONDA_PREFIX"
+else
+    read -p "Conda is not active, do you want to continue? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Ok"
+    else
+        exit 1
+    fi
+fi
 
 echo; echo "Starting LAMMPS procedure..."; echo;
 
-
-lmp_omp_edited -in in.segregation -var T $1 -var kappa $2 -var conc $3
+cd scripts
+if [ $verbose = true ]; then
+    lmp_omp_edited -in in.segregation -var gbname $name -var structure_name $structure -var conc_f $conc -var kappa_f $kappa
+else
+    $(lmp_omp_edited -in in.surface_thermal_relax -var gbname $name -var structure_name $structure -var conc_f $conc -var kappa_f $kappa)
+fi
 
 #echo; echo "LAMMPS task done, plotting..."; echo
 
